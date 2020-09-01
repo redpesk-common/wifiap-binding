4bd6b0c6249562a8b32d69ffe4dae005b6918538
salma raiss <salma.raiss@iot.bzh>
2020-09-01 10:37:50 +0200

    Fix script path
    Signed-off-by: salma raiss <salma.raiss@iot.bzh>

--- wifiApBinding/wifiAp.c
+++ wifiApBinding/wifiAp.c
@@ -48,7 +48,7 @@
 #define COMMAND_DHCP_RESTART         " DHCP_CLIENT_RESTART"
 
 //path to Wifi platform adapter shell script
-#define WIFI_SCRIPT_PATH " /tmp/LB_LINK "
+#define WIFI_SCRIPT_PATH " /tmp/LB_LINK.sh "
 
 #define MAX_SSID_LENGTH 32
 #define MIN_SSID_LENGTH 1
